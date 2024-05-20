dd409ac1fe2f73d01bcc2f3a8d5a6bbcf99a1bdc
karthikakn_069 <karthiknarayanan.cse2021@citchennai.net>
2024-05-21 05:22:46 +0530

    backcommit

--- server/hostindex.js
+++ server/hostindex.js
@@ -33,6 +33,13 @@ const tableName = "workouts";
 
 app.listen(port,()=>{
   console.log("Node running on port "+port);
+  db.connect((err) => {
+    if (err) {
+      console.error('Database connection failed:', err.stack);
+      return;
+    }
+    console.log('Connected to database.');
+  });
 })
 
 
